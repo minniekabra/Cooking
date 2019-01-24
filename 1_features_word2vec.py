3effb4e0e2c004938db05773f412feed0806d192
minniekabra <46972436+minniekabra@users.noreply.github.com>
2019-01-24 22:28:06 +0530

    Update 1_features_word2vec.py

--- 1_features_word2vec.py
+++ 1_features_word2vec.py
@@ -5,12 +5,18 @@ Created on Sun Nov 25 14:52:48 2018
 
 @author: minnie
 """
+
+"""
+This code constructs features from ingredients given using Word2Vec(By establishing relationship b/w ingredients)
+"""
+
 import gensim
 from gensim.models import Word2Vec
 
 import json
 import collections
 
+#Extracting data from given json file
 with open('/Users/minnie/Desktop/Kaggle/Cooking/Data/all/train.json','r', encoding='utf-8') as f:
     json_dict = json.load(f)
     
@@ -25,18 +31,16 @@ print(len(cuisine))
 print(len(ingredients_list))
 
 #Word2Vec
-'''
+
 model = gensim.models.Word2Vec(ingredients_list, size = 500, window = 10, min_count = 1, workers = 10)
 print('Initialized W2V')
 model.train(ingredients_list, total_examples=len(ingredients_list), epochs=10)
 print('Trained W2V', model)
 model.save('model.bin')
-'''
+
 model_save = Word2Vec.load('model.bin')
-#print(len(model_save['wheat']))
-#print(model_save['wheat'])
-#print(model_save.wv.most_similar(positive ='wheat flour'))
-#print(model_save.wv.most_similar(positive ='seasoning'))
+
+#Word2Vec example
 print(model_save.wv.similarity('salt', 'water'))
 print(model_save.wv.similarity('wheat flour', 'salt'))
 print(model_save.wv.similarity('wheat flour', 'warm water'))
@@ -54,25 +58,9 @@ for i in range(0, len(ingredients_list)):
         k = k+model_save[ingredients_list[i][j]]/len(ingredients_list[i])   
         
     ingredients_vector.append(k)
-
-k = 0
-similarity_indian = []
-
-for z in range(0, len(cuisine)):
-    if cuisine[z] == 'indian' and k == 0:
-        k = 1
-        arr1 = ingredients_vector[z]
     
-    if cuisine[z] == 'indian' and k == 1:
-        arr2 = ingredients_vector[z]
-        similarity = sum(x * y for x, y in zip(arr1, arr2))
-        similarity_indian.append(similarity)
-        arr1 = arr2
-        
-        
-print('Similarity within India is ',sum(similarity_indian)/len(similarity_indian))
-
-#Mexican & Indian
+    
+#Similarity example: b/w cuisines Mexican & Indian
 
 k = 0
 similarity_mex_indian = []
@@ -117,23 +105,4 @@ for z in range(0, len(cuisine)):
     
 print('Similarity b/w India & France ', sum(similarity_french_indian)/len(similarity_french_indian))
 
-k = 0
-similarity_french_spanish = []
-arr1 = []
-arr2 = []
 
-for z in range(0, len(cuisine)):
-    k = 0
-    if cuisine[z] == 'italian':
-        arr1 = ingredients_vector[z]
-        k = 1
-    
-    if cuisine[z] == 'french':
-        arr2 = ingredients_vector[z]
-        k = 1
-    
-    if k == 1 and len(arr1)>0 and len(arr2) > 0:
-        similarity = sum(x * y for x, y in zip(arr1, arr2))
-        similarity_french_spanish.append(similarity)
-    
-print('Similarity b/w France & Italy ', sum(similarity_french_spanish)/len(similarity_french_spanish))
